8922316b523415609978bb8c2453bb044907c623
lenny_gin <elenaginebra.z@gmail.com>
2020-11-20 23:07:07 -0600

    adding home view design

--- app/src/main/java/com/example/cookforyou/ui/pantry/PantryAdapter.kt
+++ app/src/main/java/com/example/cookforyou/ui/pantry/PantryAdapter.kt
@@ -5,8 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookforyou.R
+import com.google.firebase.firestore.DocumentSnapshot
 
-class PantryAdapter(private val pantryItem: List<PantryItem>) :
+class PantryAdapter(private val pantryItem: MutableMap<String, Any>) :
     RecyclerView.Adapter<PantryAdapter.PantryHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PantryHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -14,7 +15,7 @@ class PantryAdapter(private val pantryItem: List<PantryItem>) :
     }
 
     override fun onBindViewHolder(holder: PantryHolder, position: Int) {
-        holder.render(pantryItem[position])
+        //holder.render(pantryItem[position] as PantryItem)
     }
 
     override fun getItemCount(): Int = pantryItem.size
--- app/src/main/java/com/example/cookforyou/ui/pantry/PantryFragment.kt
+++ app/src/main/java/com/example/cookforyou/ui/pantry/PantryFragment.kt
@@ -9,18 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookforyou.R
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 private const val TAG = "PantryFragment"
 
 class PantryFragment : Fragment() {
-    private val pantryList = listOf(
-        PantryItem("Apples", 5, "unit(s)"),
-        PantryItem("Salt", 2, "Tablespoons"),
-        PantryItem("Cinnamon", 1, "Bottle"),
-    )
-
-    //private lateinit var pantryViewModel: PantryViewModel
     private lateinit var rvPantryList: RecyclerView
     var db = FirebaseFirestore.getInstance()
 
@@ -29,19 +23,16 @@ class PantryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //pantryViewModel =
-        //ViewModelProviders.of(this).get(PantryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_pantry, container, false)
         rvPantryList = root.findViewById(R.id.rvPantryList) as RecyclerView
         rvPantryList.layoutManager = LinearLayoutManager(context)
-        val adapter = PantryAdapter(pantryList)
-        rvPantryList.adapter = adapter
-        Log.w(TAG, "hmmm")
         val docRef = db.collection("pantries").document("0XYZS3PgeIW9Cw6hAvgx")
         docRef.get()
             .addOnSuccessListener { document ->
                 if (document != null) {
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    val adapter = document.data?.let { PantryAdapter(it) }
+                    rvPantryList.adapter = adapter
                 } else {
                     Log.d(TAG, "No such document")
                 }
@@ -49,7 +40,6 @@ class PantryFragment : Fragment() {
             .addOnFailureListener { exception ->
                 Log.d(TAG, "get failed with ", exception)
             }
-
         return root
     }
 }
